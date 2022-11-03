9e1edaebd6fc0d13c070ad7b1c5b5f6b89012a36
Sharper than Learning Curve <productiveAnalytics@users.noreply.github.com>
2022-11-02 21:38:57 -0500

    DDL for timescaleDB
    Use timescaleDB function: create_hypertable()

---
+++ timescaledb_hypertable_DDL.sql
@@ -0,0 +1,19 @@
+# Create regular Postgres table to hold timeseries data
+create table stocks_real_time (
+	time TIMESTAMPTZ not null,
+	symbol TEXT not null,
+	price DOUBLE precision null,
+	day_volume INT null
+);
+
+# Create Hyper Table in TimescaleDB
+select create_hypertable('stocks_real_time', 'time');
+
+# Create index
+create index ix_symbol_time on stocks_real_time (symbol, time DESC);
+
+# Create reference table
+create table company (
+	symbol text not null,
+	name text not null
+);
